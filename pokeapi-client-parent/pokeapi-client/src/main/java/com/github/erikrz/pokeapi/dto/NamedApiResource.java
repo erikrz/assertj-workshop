79fd1065796ae77be26255f8464b806fb64b0cc6
Erik Rios <erik.rios@nextiva.com>
2023-03-10 17:37:48 -0600

    Update NamedApiResource.java

--- pokeapi-client-parent/pokeapi-client/src/main/java/com/github/erikrz/pokeapi/dto/NamedApiResource.java
+++ pokeapi-client-parent/pokeapi-client/src/main/java/com/github/erikrz/pokeapi/dto/NamedApiResource.java
@@ -1,6 +1,7 @@
 package com.github.erikrz.pokeapi.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -8,7 +9,9 @@ import lombok.extern.jackson.Jacksonized;
 @Data
 @Jacksonized
 @Builder(toBuilder = true)
+@AllArgsConstructor
 public class NamedApiResource<T extends PokeApiNameResource> {
+
     private final String name;
     private final String url;
 }
