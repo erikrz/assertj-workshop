1ff25942ad5640c3b13e5a7403729d8bc08436d3
Erik Rios <erik.rz@live.com>
2023-03-11 15:37:59 -0600

    Update PokeApiClient.java

--- pokeapi-client-parent/pokeapi-client/src/main/java/com/github/erikrz/pokeapi/client/PokeApiClient.java
+++ pokeapi-client-parent/pokeapi-client/src/main/java/com/github/erikrz/pokeapi/client/PokeApiClient.java
@@ -1,8 +1,10 @@
 package com.github.erikrz.pokeapi.client;
 
 import com.github.erikrz.pokeapi.client.modules.BerriesClient;
+import com.github.erikrz.pokeapi.client.modules.ContestsClient;
+import com.github.erikrz.pokeapi.client.modules.EncountersClient;
 import com.github.erikrz.pokeapi.client.modules.PokemonClient;
 
-public interface PokeApiClient extends BerriesClient, PokemonClient {
+public interface PokeApiClient extends BerriesClient, ContestsClient, EncountersClient, PokemonClient {
 
 }
